19a720df1ba0f5bca57d8249a8d71ebd481a3587
yeeperlabratsonz <yeeperlabratson@gmail.com>
2026-07-25 22:15:44 +0000

    Clean and fix Bad Piggies and Crossy Road: remove malware/ads, copy to docs/attached_assets, add SO_REUSEADDR to server

--- server.py
+++ server.py
@@ -5,6 +5,8 @@ import os
 PORT = 5000
 DIRECTORY = "docs"
 
+socketserver.TCPServer.allow_reuse_address = True
+
 class Handler(http.server.SimpleHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, directory=DIRECTORY, **kwargs)
