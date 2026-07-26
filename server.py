d745aa0f5f466aea18ed824f1a68b3f09bdcf077
yeeperlabratsonz <yeeperlabratson@gmail.com>
2026-07-26 00:56:59 +0000

    Add black loading overlay to hide Bad Piggies splash, add cache-busting and no-cache headers

--- server.py
+++ server.py
@@ -11,6 +11,12 @@ class Handler(http.server.SimpleHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, directory=DIRECTORY, **kwargs)
 
+    def end_headers(self):
+        self.send_header("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0")
+        self.send_header("Pragma", "no-cache")
+        self.send_header("Expires", "0")
+        super().end_headers()
+
     def log_message(self, format, *args):
         pass  # suppress request logs
 
